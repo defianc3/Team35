ba8e62140c32b317eeb01a020289b6599a00a857
rwool <dudude@neo.tamu.edu>
2013-03-13 22:32:08 -0500

    Beginning implementation of MinimaxTree class. Added Evaluatable interface.

--- src/MiniMaxTree.java
+++ src/MiniMaxTree.java
@@ -1,4 +1,42 @@
+import java.util.ArrayList;
 
-public class MiniMaxTree {
+public class MiniMaxTree<T extends Evaluatable> {
+	Node root;
 	
+	public class Node {
+		Node parent;
+		T state;
+		int utilityValue = 0;
+		ArrayList<Node> children;
+		
+		Node(Node _parent, T _state) {
+			parent = _parent;
+			state = _state;
+		}
+		
+		void addChild(Node _child) {
+			children.add(_child);
+		}
+		
+		void generateUtilityValue() {
+			utilityValue = state.evaluate();
+		}
+	}
+	
+	MiniMaxTree(T state) {
+		root = new Node(null, state);
+	}
+	
+	/* TODO Needed? Or use lazy evaluation? */
+	private void populateToDepth(int depth) {
+		
+	}
+	
+	void alphaCut() {
+		
+	}
+	
+	void betaCut() {
+		
+	}
 }
